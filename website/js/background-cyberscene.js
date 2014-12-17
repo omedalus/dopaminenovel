3036b35b7d2be5e08b695a59d2a47b70af783fee
Mikhail Voloshin <mvol@live.com>
2014-12-17 18:08:28 -0500

    done for the day

--- website/js/background-cyberscene.js
+++ website/js/background-cyberscene.js
@@ -19,25 +19,42 @@ var scaleMists;
   var NUM_MISTS = 90;
   var MIST_SIZE = .2;
   var MIST_DRIFT_RATE_PER_FRAME = .0003;
+  var FLASH_PROBABILITY_PER_FRAME = 0.001;
+  var FLASH_DURATION_FRAMES = 100;
 
   var transgreen = new Color(0, .6, 0, .4);
   var transparent = new Color(0, 0, 0, 0);
 
+  var fillColorGreenBubble = {
+    gradient: {
+      stops: [['green', 0], [transgreen, .2], [transgreen, .3], ['black', .9], [transparent, 1]],
+      radial: true
+    },
+    origin: [0, 0],
+    destination: [50, 0]
+  };
+
+  var fillColorWhiteFlash = {
+    gradient: {
+      stops: [['white', 0], ['white', .4], [transparent, 1]],
+      radial: true
+    },
+    origin: [0, 0],
+    destination: [50, 0]
+  };
+
+  
   var mistPath = new Path.Circle({
     center: [0, 0],
-    radius: 100,    
-    
-    fillColor: {
-      gradient: {
-        stops: [['green', 0], [transgreen, .1], [transparent, 1]],
-        radial: true
-      },
-      origin: [0, 0],
-      destination: [100, 0]
-    }
+    radius: 50,
+    fillColor: fillColorGreenBubble
   });
   
+  var mistWhiteFlashPath = mistPath.clone();
+  mistWhiteFlashPath.fillColor = fillColorWhiteFlash;
+  
   var mistSymbol = new Symbol(mistPath);
+  var mistFlashSymbol = new Symbol(mistWhiteFlashPath);
   
   for (var iMist = 0; iMist < NUM_MISTS; iMist++) {
     var mistObj = {};
@@ -47,12 +64,15 @@ var scaleMists;
     mistObj.viewportScale.x -= 1;
     
     mistObj.instance = mistSymbol.place([0, 0]);
-    debugger;
+    mistObj.flashInstance = mistFlashSymbol.place([0, 0]);
+    mistObj.flashInstance.opacity = 0;
+    mistObj.flashProgress = 0;
     
     mistObj.drift = Math.random();
 
     mistObj.objectScale = .5 + Math.random();
     mistObj.instance.scale(mistObj.objectScale);
+    mistObj.flashInstance.scale(mistObj.objectScale);
     
     Mists.push(mistObj);
   }
@@ -61,10 +81,22 @@ var scaleMists;
     for (var iMist = 0; iMist < Mists.length; iMist++) {
       var mistObj = Mists[iMist];
       mistObj.instance.position = mistObj.viewportScale * view.size;
-      
+      mistObj.flashInstance.position = mistObj.viewportScale * view.size;
+ 
+      // Drift.
       mistObj.viewportScale.x += MIST_DRIFT_RATE_PER_FRAME * mistObj.drift;
       if (mistObj.viewportScale.x > 2) {
         mistObj.viewportScale.x -= 3;
+      }
+      
+      // Maybe flash.
+      if (mistObj.flashProgress === 0 && 
+          Math.random() < FLASH_PROBABILITY_PER_FRAME) {
+        mistObj.flashProgress = 1;
+      }
+      
+      if (mistObj.flashProgress > 0) {
+        
       }
     }
   };
@@ -78,6 +110,7 @@ var scaleMists;
       var mistObj = Mists[iMist];
       
       mistObj.instance.scale(newSizeScale / currentSizeScale);
+      mistObj.flashInstance.scale(newSizeScale / currentSizeScale);
     }
     
     currentSizeScale = newSizeScale;

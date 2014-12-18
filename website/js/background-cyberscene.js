7d3707e9fc34629819e44d52dd739730c4b40599
Mikhail Voloshin <mvol@live.com>
2014-12-18 14:21:56 -0500

    success

--- website/js/background-cyberscene.js
+++ website/js/background-cyberscene.js
@@ -136,75 +136,196 @@ String.prototype.hashCode = function() {
 
 (function() {
   var VERTEX_BLOCK_SIZE = 12;
-  var FLASH_PROBABILITY_PER_FRAME = 0.00005;
-  var FLASH_PROBABILITY_CHANGE_DIR = 0.1;
+  var FLASH_COUNT = 200;
+  var FLASH_FRAMES_PER_MOVE = 2;
+  var FLASH_DEATH_PROBABILITY_PER_FRAME = .01;
 
-  var vertices;
-
+  var flashes = [];
+  
   // Use a date string prefix in lieu of a random function.
   var today = new Date();
   var salt = '' + today.getYear() + today.getMonth() + today.getDate();
+  
+  var determineVertexDirections = function(x, y) {
+    var vxstr = salt + '(' + x + ',' + y + ')';
+  
+    return {
+      diagback: Math.abs((vxstr + 'diagback').hashCode()) % 20 === 0,
+      back: Math.abs((vxstr + 'back').hashCode()) % 20 < 8,
+      up: Math.abs((vxstr + 'up').hashCode()) % 20 < 8,
+      diagfwd: Math.abs((vxstr + 'diagfwd').hashCode()) % 20 === 0
+    };
+  };
+  
+  var determineDirectionsFromPoint = function(x, y, lastDirection) {
+    var retval = [];
+    var linesFromHere = determineVertexDirections(x, y);
+    if (linesFromHere.diagback) {
+      retval.push({x: -VERTEX_BLOCK_SIZE, y: -VERTEX_BLOCK_SIZE});
+    }
+    if (linesFromHere.back) {
+      retval.push({x: -VERTEX_BLOCK_SIZE, y: 0});
+    }
+    if (linesFromHere.up) {
+      retval.push({x: 0, y: -VERTEX_BLOCK_SIZE});
+    }
+    if (linesFromHere.diagfwd) {
+      retval.push({x: VERTEX_BLOCK_SIZE, y: -VERTEX_BLOCK_SIZE});
+    }
+    
+    if (determineVertexDirections(x + VERTEX_BLOCK_SIZE, y).back) {
+      retval.push({x: VERTEX_BLOCK_SIZE, y: 0});
+    }
+
+    if (determineVertexDirections(x - VERTEX_BLOCK_SIZE, y + VERTEX_BLOCK_SIZE).diagfwd) {
+      retval.push({x: -VERTEX_BLOCK_SIZE, y: VERTEX_BLOCK_SIZE});
+    }
+    
+    if (determineVertexDirections(x, y + VERTEX_BLOCK_SIZE).up) {
+      retval.push({x: 0, y: VERTEX_BLOCK_SIZE});
+    }
+    
+    if (determineVertexDirections(x + VERTEX_BLOCK_SIZE, y + VERTEX_BLOCK_SIZE).diagback) {
+      retval.push({x: VERTEX_BLOCK_SIZE, y: VERTEX_BLOCK_SIZE});
+    }
+
+    if (lastDirection) {
+      var filteredRetval = [];
+      
+      for (var iDir in retval) {
+        var dir = retval[iDir];
+        if (dir.x === lastDirection.x && dir.y === lastDirection.y) {
+          return [lastDirection];
+        }
+        
+        if (dir.x === -lastDirection.x && dir.y === -lastDirection.y) {
+          // Exact opposite direction. Don't include.
+          continue;
+        }
+        
+        filteredRetval.push(dir);
+      }
+      
+      retval = filteredRetval;
+    }
+    
+    return retval;
+  };
   
   setupCircuitGrid = function() {
     var canvas = layers.GRID_STATIC.canvas;
     var drawingCtx = layers.GRID_STATIC.drawingCtx;
     
-    vertices = {};
-
     for (var y = 0; y < canvas.height + VERTEX_BLOCK_SIZE; y += VERTEX_BLOCK_SIZE) {
       for (var x = 0; x < canvas.width + VERTEX_BLOCK_SIZE; x += VERTEX_BLOCK_SIZE) {
-        var vertexObj = {};
         
-        vertexObj.position = {x: x, y: y};
-        vertexObj.key = '(' + x + ',' + y + ')';
-        vertices[vertexObj.key] = vertexObj;
+        var lines = determineVertexDirections(x, y);
         
-        var vxstr = salt + vertexObj.key;
+        var renderX = x - (VERTEX_BLOCK_SIZE / 2);
+        var renderY = y - (VERTEX_BLOCK_SIZE / 2);
         
-        vertexObj.lines = {
-          diagback: Math.abs((vxstr + 'diagback').hashCode()) % 20 === 0,
-          back: Math.abs((vxstr + 'back').hashCode()) % 20 < 8,
-          up: Math.abs((vxstr + 'up').hashCode()) % 20 < 8,
-          diagfwd: Math.abs((vxstr + 'diagfwd').hashCode()) % 20 === 0
-        };
+        drawingCtx.beginPath();
         
+        if (lines.diagback) {
+          drawingCtx.moveTo(renderX, renderY);
+          drawingCtx.lineTo(renderX - VERTEX_BLOCK_SIZE, renderY - VERTEX_BLOCK_SIZE);
+        }
         
-        vertexObj.flashing = false;
+        if (lines.back) {
+          drawingCtx.moveTo(renderX, renderY);
+          drawingCtx.lineTo(renderX - VERTEX_BLOCK_SIZE, renderY);
+        }
+
+        if (lines.up) {
+          drawingCtx.moveTo(renderX, renderY);
+          drawingCtx.lineTo(renderX, renderY - VERTEX_BLOCK_SIZE);
+        }
+
+        if (lines.diagfwd) {
+          drawingCtx.moveTo(renderX, renderY);
+          drawingCtx.lineTo(renderX + VERTEX_BLOCK_SIZE, renderY - VERTEX_BLOCK_SIZE);
+        }
+        
+        drawingCtx.lineWidth = 2;
+        drawingCtx.strokeStyle = 'rgba(0, 40, 0, 1)';
+              
+        drawingCtx.stroke();
       }
     }
+  };
+  
+  animateCircuitGrid = function() {
+    var canvas = layers.GRID_ANIMATED.canvas;
+    var drawingCtx = layers.GRID_ANIMATED.drawingCtx;
+    
+    drawingCtx.clearRect(0, 0, canvas.width, canvas.height);
 
-    for (var vxKey in vertices) {
-      vertexObj = vertices[vxKey];
+    for (var iFlash = 0; iFlash < FLASH_COUNT; iFlash++) {
+      var flash = flashes[iFlash];
+      if (!flash) {
+        flash = {};
+        flashes[iFlash] = flash;
+      }
       
-      var renderX = vertexObj.position.x - (VERTEX_BLOCK_SIZE / 2);
-      var renderY = vertexObj.position.y - (VERTEX_BLOCK_SIZE / 2);
+      if (!flash.position) {
+        flash.position = {
+          x: Math.random() * canvas.width,
+          y: Math.random() * canvas.height,          
+        };
+        flash.position.x -= flash.position.x % VERTEX_BLOCK_SIZE;
+        flash.position.y -= flash.position.y % VERTEX_BLOCK_SIZE;
+      }
+
+      if (!flash.colorPrefix) {
+        var colorChooser = Math.random();
+        if (colorChooser < .6) {
+          flash.colorPrefix = 'rgba(120, 120, 0, ';
+        } else if (colorChooser < .8) {
+          flash.colorPrefix = 'rgba(255, 0, 0, ';
+        } else {
+          flash.colorPrefix = 'rgba(0, 255, 255, ';
+        }
+      }
+      
+      var validDirectionsFromHere =
+        determineDirectionsFromPoint(flash.position.x, flash.position.y, flash.direction);
+      
+      if (validDirectionsFromHere.length === 0 || Math.random() < FLASH_DEATH_PROBABILITY_PER_FRAME) {
+        flashes[iFlash] = null;
+        continue;
+      }
+      
+      flash.direction = 
+          validDirectionsFromHere[Math.floor(Math.random() * validDirectionsFromHere.length)];
+      
+      if (!flash.moveProgress) {
+        flash.moveProgress = 0;
+      }
+
+      var moveProgRatio = flash.moveProgress / FLASH_FRAMES_PER_MOVE;
+      
+      var renderX = flash.position.x + (flash.direction.x * moveProgRatio) - (VERTEX_BLOCK_SIZE / 2);
+      var renderY = flash.position.y + (flash.direction.y * moveProgRatio) - (VERTEX_BLOCK_SIZE / 2);
+      var radius = 10;
       
       drawingCtx.beginPath();
-      
-      if (vertexObj.lines.diagback) {
-        drawingCtx.moveTo(renderX, renderY);
-        drawingCtx.lineTo(renderX - VERTEX_BLOCK_SIZE, renderY - VERTEX_BLOCK_SIZE);
-      }
-      
-      if (vertexObj.lines.back) {
-        drawingCtx.moveTo(renderX, renderY);
-        drawingCtx.lineTo(renderX - VERTEX_BLOCK_SIZE, renderY);
-      }
+      drawingCtx.arc(renderX, renderY, radius, 0, 2 * Math.PI, false);
 
-      if (vertexObj.lines.up) {
-        drawingCtx.moveTo(renderX, renderY);
-        drawingCtx.lineTo(renderX, renderY - VERTEX_BLOCK_SIZE);
-      }
-
-      if (vertexObj.lines.diagfwd) {
-        drawingCtx.moveTo(renderX, renderY);
-        drawingCtx.lineTo(renderX + VERTEX_BLOCK_SIZE, renderY - VERTEX_BLOCK_SIZE);
-      }
+      var gradient = drawingCtx.createRadialGradient(renderX, renderY, 0, renderX, renderY, radius);
+      gradient.addColorStop(0, flash.colorPrefix + '1)');
+      gradient.addColorStop(.1, flash.colorPrefix + '.6)');
+      gradient.addColorStop(1, 'rgba(0, 0, 0, 0)');
       
-      drawingCtx.lineWidth = 2;
-      drawingCtx.strokeStyle = 'rgba(0, 60, 0, 1)';
-            
-      drawingCtx.stroke();
+      drawingCtx.fillStyle = gradient;
+      drawingCtx.fill();
+      
+      flash.moveProgress++;
+      
+      if (flash.moveProgress >= FLASH_FRAMES_PER_MOVE) {
+        flash.moveProgress = 0;
+        flash.position.x += flash.direction.x;
+        flash.position.y += flash.direction.y;        
+      }
     }
   };
 
@@ -212,6 +333,7 @@ String.prototype.hashCode = function() {
 
 
 function onFrame(event) {
+  animateCircuitGrid();
   animateBubbles();
 };
 
@@ -262,6 +384,7 @@ $(document).ready(function() {
   }
   
   layers.GRID_STATIC.canvas.style.background = 'black';
+  layers.GRID_ANIMATED.canvas.style.opacity = .5;
 
   $(window).resize();  
   tickFrame();
